0b0415e8a7e1350c86111f438cc1224f7466f987
Stefan Martynkiw <linster247@gmail.com>
2016-02-26 09:51:40 -0700

    Added some magic parallel blocks and some criticals. Don't know yet how the variable sharing's gonna work out.

--- main.c
+++ main.c
@@ -7,6 +7,10 @@
 #define TOL 0.0005
 
 int main(int argc, char * argv[]) {
+
+
+  int a_num_threads = atoi(argv[1]);
+
   int i, j, k, size;
   double ** Au;
   double * X;
@@ -27,6 +31,11 @@ int main(int argc, char * argv[]) {
   if(size == 1) {
     X[0] = Au[0][1] / Au[0][0];
   } else {
+
+
+#pragma omp parallel num_threads(a_num_threads)
+{
+
     /*Gaussian elimination*/
     for(k = 0; k < size - 1; ++k) {
       /*Pivoting*/
@@ -50,11 +59,19 @@ int main(int argc, char * argv[]) {
       for(i = k + 1; i < size; ++i) {
 	temp = Au[index[i]][k] / Au[index[k]][k];
 
+# pragma omp critical
+{
 	for(j = k; j < size + 1; ++j) {
 	  Au[index[i]][j] -= Au[index[k]][j] * temp;
 	}
+} /* End Critical */
+
       }       
     }
+}
+
+#pragma omp parallel num_threads(a_num_threads)
+{
 
     /*Jordan elimination*/
     for(k = size - 1; k > 0; --k) {
@@ -65,11 +82,17 @@ int main(int argc, char * argv[]) {
       } 
     }
 
+
+#pragma omp critical {
     /*solution*/
     for(k=0; k< size; ++k) {
       X[k] = Au[index[k]][size] / Au[index[k]][k];
     }
+} /* End Critical */
+
   }
+
+}
 
   DestroyVec(X);
   DestroyMat(Au, size);
